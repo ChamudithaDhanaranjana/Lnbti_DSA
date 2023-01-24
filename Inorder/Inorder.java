025bef210678b88e6f8de65a801e1f7c0569442c
Kavi1008 <kavi1008dk@gmail.com>
2023-01-25 05:28:43 +0530

    Inorder class created

---
+++ Inorder/Inorder.java
@@ -0,0 +1,63 @@
+class Inorder
+{
+	Node root;
+	class Node // used as a nested class since node is created inside a tree
+	{
+		int value;
+		Node LeftChild;
+		Node RightChild;
+		
+		Node (int item) {
+		
+			this.value = item;
+			
+		}
+	}
+	
+	public void inorder (Node node)
+	{
+		if (node!=null)
+		{
+			
+			inorder (node.LeftChild);
+			System.out.print (node.value);
+			inorder (node.RightChild);
+		}
+	}
+	
+	
+	public Node insert (Node node , int value) {
+		
+		if (node == null){
+			return new Node (value);
+		}
+		if (value < node.value){
+			
+			node.LeftChild = insert (node.LeftChild, value);
+		}
+		else if (value> node.value) {
+			node.RightChild = insert (node.RightChild,value);
+		}
+		return node;
+	}
+
+	
+	public static void main (String args[]) {
+		Inorder bst = new Inorder ();
+		Node root = bst.root;		
+		root = bst.insert (bst.root ,12);
+		root = bst.insert (root , 26);
+		root = bst.insert (root , 80);
+		root = bst.insert (root , 54);
+		root = bst.insert (root, 38);
+		root = bst.insert (root, 42);
+		root = bst.insert (root, 65);
+		root = bst.insert (root, 10);
+		root = bst.insert (root, 55);
+		root = bst.insert (root, 43);
+		
+		
+		
+	}
+	
+}

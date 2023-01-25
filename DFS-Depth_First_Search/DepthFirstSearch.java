afe8b965d7da5e3037216fc9ded5df49ef3e14c9
Mindi <mindidp2662@gmail.com>
2023-01-25 18:14:03 +0530

    updated the DFS java code

--- DFS-Depth_First_Search/DepthFirstSearch.java
+++ DFS-Depth_First_Search/DepthFirstSearch.java
@@ -10,5 +10,26 @@ class DepthFirstSearch {
 			left = right = null;
 		}
 	}
+	
+	public void depthFirstSearch(Node root) {
+		if(root == null) {
+			return;
+		}
+		
+		Stack<Node> nodeStack = new Stack<Node>();
+		nodeStack.push(root);
+		
+		while(!nodeStack.isEmpty()) {
+			Node node = nodeStack.pop();
+			System.out.print(node.data + " ");
+			
+			if(node.right != null) {
+				nodeStack.push(node.right);
+			}
+			if(node.left != null) {
+				nodeStack.push(node.left);
+			}
+		}
+	}
 }
 

a3ea9b59c6def87ab394d4f750663bb900d5c631
Mindi <mindidp2662@gmail.com>
2023-01-25 14:16:05 +0530

    created class node

--- DFS-Depth_First_Search/DepthFirstSearch.java
+++ DFS-Depth_First_Search/DepthFirstSearch.java
@@ -1,3 +1,14 @@
 class DepthFirstSearch {
+	
+	private class Node {
+		int data;
+		Node left;
+		Node right;
+		
+		private Node(int value) {
+			data = value;
+			left = right = null;
+		}
+	}
 }
 

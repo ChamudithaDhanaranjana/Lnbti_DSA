3366adda18b7baa40d261e0367230b0db0c2d9b4
Mindi <mindidp2662@gmail.com>
2023-01-25 11:12:56 +0530

    updated the code

--- Selection_Sort/SelectionSort.java
+++ Selection_Sort/SelectionSort.java
@@ -21,7 +21,7 @@ class SelectionSort
 	void printArray(int arr[])
     {
         int n = arr.length;
-        for (int i=1; i<n; ++i)
+        for (int i = 0; i < n; ++i)
             System.out.print(arr[i]+" ");
         System.out.println();
     }

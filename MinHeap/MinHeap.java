4f76481c3985fc92ef9d5dd3359eaf608314327f
Kavi1008 <kavi1008dk@gmail.com>
2023-01-25 05:45:05 +0530

    Created main class

--- MinHeap/MinHeap.java
+++ MinHeap/MinHeap.java
@@ -104,6 +104,26 @@ public class MinHeap
         minHeapify(FRONT);
         return popped;
     }
+	    
+	public static void main(String []args)
+    {
+        System.out.println("The Min Heap is ");
+        MinHeap minHeap = new MinHeap(15);
+        minHeap.insert(5);
+        minHeap.insert(3);
+        minHeap.insert(17);
+        minHeap.insert(10);
+        minHeap.insert(84);
+        minHeap.insert(19);
+        minHeap.insert(6);
+        minHeap.insert(22);
+        minHeap.insert(9);
+        minHeap.minHeap();
+ 
+        minHeap.print();
+        System.out.println("The Minimum value = " + minHeap.remove());
+    }
+
  
    
 }

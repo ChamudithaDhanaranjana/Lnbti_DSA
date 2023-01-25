1b79600e8eb64165c10f8bf51300b33f074e1d62
keshihasithangi <hasithikeshi17@gmail.com>
2023-01-25 13:58:50 +0530

    Updated the code

--- FloydWarshall_Algorithm/FloydWarshallAlgorithm.java
+++ FloydWarshall_Algorithm/FloydWarshallAlgorithm.java
@@ -1,4 +1,30 @@
 class FloydWarshallAlgorithm
 {
-	
+	public static final int INF = 999, Vertices = 4; //static int - variable remains in memory while the program is running
+	//999 is the value given to the infinite values
+  
+	void floydWarshallAlgorithm(int path[][]) // Implementing the algorithm
+	{
+		int matrix[][] = new int[Vertices][Vertices];
+		int i, j, k;
+		
+		for (i = 0; i < Vertices; i++)
+			for (j = 0; j < Vertices; j++)
+				matrix[i][j] = path[i][j];
+
+		
+		for (k = 0; k < Vertices; k++) 
+		{
+			for (i = 0; i < Vertices; i++) 
+			{
+				for (j = 0; j < Vertices; j++) 
+				{
+					if (matrix[j][k] + matrix[k][j] < matrix[i][j])
+					{
+						matrix[i][j] = matrix[i][k] + matrix[k][j];
+					}
+				}
+			}
+		}
+	}
 }

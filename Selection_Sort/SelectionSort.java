648b481cbc396357a3df19022a3c51527171a1fe
Mindi <mindidp2662@gmail.com>
2023-01-25 11:19:49 +0530

    changed the code

--- Selection_Sort/SelectionSort.java
+++ Selection_Sort/SelectionSort.java
@@ -5,7 +5,7 @@ class SelectionSort
     {
         int n = arr.length;
  
-        for (int i = 0; i < n-1; i++)
+        for (int i = 0; i < n; i++)
         {
             // Find the minimum element in unsorted array
             int min_index = i;
@@ -13,7 +13,7 @@ class SelectionSort
                 if (arr[j] < arr[min_index])
                     min_index = j;
 
-            int temp = arr[min_indx];
+            int temp = arr[min_index];
             arr[min_index] = arr[i];
             arr[i] = temp;
         }
